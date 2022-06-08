8e27527d00512bc0ee42c0f351b6f3b20b2e0215
AdamHebby <adamhebden56@gmail.com>
2022-06-09 00:39:47 +0100

    Deletes Client, updates dependencies, deletes unneeded tests and classes

--- examples/query_builder_example.php
+++ examples/query_builder_example.php
@@ -2,15 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use GraphQL\Client;
-use GraphQL\Exception\QueryError;
 use GraphQL\QueryBuilder\QueryBuilder;
-
-// Create Client object to contact the GraphQL endpoint
-$client = new Client(
-    'https://graphql-pokemon.now.sh/',
-    []  // Replace with array of extra headers to be sent with request for auth or other purposes
-);
 
 // Create the GraphQL query
 $builder = (new QueryBuilder('pokemon'))
@@ -42,24 +34,3 @@ $builder = (new QueryBuilder('pokemon'))
                     )
             )
     );
-
-// Run query to get results
-try {
-    $results = $client->runQuery($builder);
-}
-catch (QueryError $exception) {
-
-    // Catch query error and desplay error details
-    print_r($exception->getErrorDetails());
-    exit;
-}
-
-// Display original response from endpoint
-var_dump($results->getResponseObject());
-
-// Display part of the returned results of the object
-var_dump($results->getData()->pokemon);
-
-// Reformat the results to an array and get the results of part of the array
-$results->reformatResults(true);
-print_r($results->getData()['pokemon']);
